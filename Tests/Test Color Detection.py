5fe72cf3e2267392b5af1a0525443351113cf170
DiegoCorre21 <104682170+DiegoCorre21@users.noreply.github.com>
2023-06-21 18:38:39 +0800

    Update Test Color Detection.py

--- Tests/Test Color Detection.py
+++ Tests/Test Color Detection.py
@@ -6,7 +6,7 @@ lowera = np.array([5, 90, 100])  # Establish the lower and upper HSV values
 uppera = np.array([50, 255, 255])
 cap = cv2.VideoCapture(1, cv2.CAP_DSHOW)
 while True:
-    frame = cv2.imread("figure 3.jpg")
+    _, frame = cap.read()
     HSV_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)  # Transform to HSV coordinates
     mask = cv2.inRange(HSV_frame, lowera, uppera)
     cv2.imshow("mask", mask)
@@ -15,3 +15,4 @@ while True:
     if key == ord("q"):
         cv2.imwrite("mask.png", mask)
         break
+
