ea5dba522b4992762db7fb44982fcb5e0c679af1
DiegoCorre21 <104682170+DiegoCorre21@users.noreply.github.com>
2023-06-21 20:22:54 +0800

    Create camera calibration calculation.py

---
+++ Camera Calibration/camera calibration calculation.py
@@ -0,0 +1,77 @@
+import numpy as np
+import cv2 as cv
+import glob
+
+
+
+################ FIND CHESSBOARD CORNERS - OBJECT POINTS AND IMAGE POINTS #############################
+
+
+chessboardSize = (7, 5)
+frameSize = (640, 480)
+
+
+
+# termination criteria
+criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+objp = np.zeros((chessboardSize[0] * chessboardSize[1], 3), np.float32)
+objp[:, :2] = np.mgrid[0:chessboardSize[0], 0:chessboardSize[1]].T.reshape(-1,2)
+
+size_of_chessboard_squares_mm = 30
+objp = objp * size_of_chessboard_squares_mm
+print(objp)
+
+# Arrays to store object points and image points from all the images.
+objpoints = [] # 3d point in real world space
+imgpoints = [] # 2d points in image plane.
+
+
+images = glob.glob('images/*.png')
+
+for image in images:
+
+    img = cv.imread(image)
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+    # Find the chess board corners
+    ret, corners = cv.findChessboardCorners(gray, chessboardSize, None)
+
+    # If found, add object points, image points (after refining them)
+    if ret == True:
+        objpoints.append(objp)
+        corners2 = cv.cornerSubPix(gray, corners, (11,11), (-1,-1), criteria)
+        imgpoints.append(corners)
+
+        # Draw and display the corners
+        cv.drawChessboardCorners(img, chessboardSize, corners2, ret)
+        cv.imshow('img', img)
+        cv.waitKey(10)
+
+
+cv.destroyAllWindows()
+
+
+
+
+############## CALIBRATION #######################################################
+ret, cameraMatrix, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, frameSize, None, None)
+print(f"Tvec: \n{np.round(tvecs, 5)}")
+print(cameraMatrix)
+# Reprojection Error
+mean_error = 0
+np.savez(
+    f"MultiMatrix_fixed_640_480",
+    camMatrix=cameraMatrix,
+    distCoef=dist,
+    rVector=rvecs,
+    tVector=tvecs,
+)
+for i in range(len(objpoints)):
+    imgpoints2, _ = cv.projectPoints(objpoints[i], rvecs[i], tvecs[i], cameraMatrix, dist)
+    error = cv.norm(imgpoints[i], imgpoints2, cv.NORM_L2)/len(imgpoints2)
+    mean_error += error
+
+print( "total error: {}".format(mean_error/len(objpoints)) )
